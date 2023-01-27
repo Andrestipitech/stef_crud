022c964d524fe1668f677ea8fa756a2795df26b9
AndresAlvaradow <andresalvarado34@gmail.com>
2023-01-26 21:33:58 -0500

    insertar y eliminar

--- apps/auto/urls.py
+++ apps/auto/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'auto'
 urlpatterns = [
     path('', views.home, name='index'),
+    path('insertar_a/', views.insertar_auto, name='crear_auto'),
+    path('eliminar_a/<int:id_auto>', views.eliminar_auto, name='eliminar_auto'),
 
 ]
--- apps/auto/views.py
+++ apps/auto/views.py
@@ -10,5 +10,16 @@ def home(request):
     }
     return render(request, 'auto/auto_admin.html', context)
 
-def prueba():
-    print("hola")
+def insertar_auto(request):
+    marca_i = request.POST['marca']
+    modelo_i = request.POST['modelo']
+    cc_i = request.POST['cc']
+    detalle_i = request.POST['descripcion']
+    Auto_i = Auto(marca= marca_i, modelo = modelo_i, cc=cc_i, especificaciones=detalle_i)
+    Auto_i.save()
+    return redirect('auto:index')
+
+def eliminar_auto(request, id_auto):
+    Auto_del = Auto.objects.get(idpbv=id_auto)
+    Auto_del.delete()
+    return redirect('auto:index')
