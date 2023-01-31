38332152847ce602a846d66c579749a3c0496467
AndresAlvaradow <andresalvarado34@gmail.com>
2023-01-30 21:33:30 -0500

    actualizar auto

--- apps/auto/urls.py
+++ apps/auto/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='index'),
     path('insertar_a/', views.insertar_auto, name='crear_auto'),
     path('eliminar_a/<int:id_auto>', views.eliminar_auto, name='eliminar_auto'),
-
+    path('actualizar_a/<int:id_auto>', views.actualizar_form, name='act_auto_form'),
+    path('actualiza/', views.actualizar, name='actualiza_auto'),
 ]
--- apps/auto/views.py
+++ apps/auto/views.py
@@ -23,3 +23,28 @@ def eliminar_auto(request, id_auto):
     Auto_del = Auto.objects.get(idpbv=id_auto)
     Auto_del.delete()
     return redirect('auto:index')
+
+def actualizar_form(request, id_auto):
+    lista_autos = Auto.objects.all() # de obtener toda la informacion de la tabla
+    auto_data = Auto.objects.get(idpbv=id_auto)
+    context = {
+        'autos': lista_autos,
+        'actualiza':auto_data
+    }
+    return render(request, 'auto/auto_admin.html', context)
+
+def actualizar(request):
+    a_id_a = request.POST['idAuto']
+    a_marca_a = request.POST['marca']
+    a_modelo_a = request.POST['modelo']
+    a_cc_a = request.POST['cc']
+    a_des_a = request.POST['descripcion']
+
+    act_auto = Auto.objects.get(idpbv=a_id_a)
+
+    act_auto.marca = a_marca_a
+    act_auto.modelo = a_modelo_a
+    act_auto.cc = a_cc_a
+    act_auto.especificaciones = a_des_a
+    act_auto.save()
+    return redirect('auto:index')
