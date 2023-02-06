7f5a3e6dbc71d9fd49b462a63e0ae73e5be82463
AndresAlvaradow <andresalvarado34@gmail.com>
2023-02-06 06:58:09 -0500

    primera configuracion admin

--- apps/auto/admin.py
+++ apps/auto/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . import models
+#from apps.auto.models import Auto
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+
+class AutoCategoria(resources.ModelResource):
+    class Meta:
+        model = models.Auto
+        import_id_fields =('idpbv',)
+
+class DataAuto(ImportExportModelAdmin,admin.ModelAdmin):
+    search_fields =['idpbv']
+    list_display =('idpbv', 'marca', 'modelo', 'cc', 'especificaciones')
+    resource_class = AutoCategoria
+
+admin.site.register(models.Auto, DataAuto)
--- stefanyCrud/settings.py
+++ stefanyCrud/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.auto',
+    'import_export',
 ]
 
 MIDDLEWARE = [
